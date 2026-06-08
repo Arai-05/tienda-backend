f12a1e5a07f5a250693d26ca11219624632297cc
Arai-05 <benj.aravenam@duocuc.cl>
2026-06-07 22:57:24 -0400

    comentario

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+//sas
 const {
   DB_HOST = "10.0.30.196", // acá colocar la IP Privada EC2 DB
   DB_USER = "root",
